be7701fb8cdd8c575644a6575e8d4d16d5219c88
jose nunes <josenunesra@gmail.com>
2018-03-02 15:40:01 -0300

    aula 6 - sistema de rotas

--- Aprenda PHP na Pratica - Curso/Api/api.php
+++ Aprenda PHP na Pratica - Curso/Api/api.php
@@ -4,5 +4,27 @@ include('configuracao.php');
 include('usuarios.php');
 
 function getPagina(){
-    return include('Paginas/home.php');
+    $url = $_SERVER['REQUEST_URI'];
+    $metodo = $_SERVER['REQUEST_METHOD'];
+
+    if($metodo == "GET"){	
+		switch($url){
+			case "/":
+				include('Paginas/home.php');
+				break;
+			case "/home":
+				include('Paginas/home.php');
+				break;
+			case "/sobre":
+				include('Paginas/sobre.php');
+				break;
+			case "/contato":
+				include('Paginas/contato.php');
+				break;
+			default:
+				include('Paginas/home.php');
+				break;
+		}
+	}
+	//var_dump($_SERVER);    
 }
--- Aprenda PHP na Pratica - Curso/Layouts/site.php
+++ Aprenda PHP na Pratica - Curso/Layouts/site.php
@@ -1,9 +1,10 @@
 <?php
 
+// inclui o topo da página html
 include('_include/_topo.php');
 
+// inclui o conteúdo de alguma página
 getPagina();
 
+// inclui o rodape da página html
 include('_include/_rodape.php');
-
-?>
