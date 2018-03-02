56e364bd9f354f1feabb9c54af6bafcb716091bf
jose nunes <josenunesra@gmail.com>
2018-03-02 14:00:33 -0300

    aula 4 - for

--- Aprenda PHP na Pratica - Curso/aula 3 - foreach e for.php
+++ Aprenda PHP na Pratica - Curso/aula 3 - foreach e for.php
@@ -9,9 +9,9 @@ function getInfo($atributo){
 
 function getUsuarios(){
 	$usuarios = [
-			["nome"=>"Guilherme", "email"=>"gui@mail.com"],
-			["nome"=>"Maria", "email"=>"mari@mail.com"],
-			["nome"=>"Pedro", "email"=>"pedro@mail.com"]
+		["nome"=>"Guilherme", "email"=>"gui@mail.com"],
+		["nome"=>"Maria", "email"=>"mari@mail.com"],
+		["nome"=>"Pedro", "email"=>"pedro@mail.com"]
 	];
 
 	return $usuarios;
@@ -22,10 +22,24 @@ function exibeUsuario(){
 	$html = "";
 
 	foreach($usuarios as $chave => $usuario){
-			$nome  = $usuario["nome"];
-			$email = $usuario["email"];
+		$nome  = $usuario["nome"];
+		$email = $usuario["email"];
 
-			$html .= "<li>Nome: $nome - Email: $email </li>";
+		$html .= "<li>Nome: $nome - Email: $email </li>";
+	}
+
+	return $html;
+}
+
+function exibeUsuarioComFor(){
+	$usuarios = getUsuarios();
+	$html = "";
+
+	for($y = 0; $y < count($usuarios); $y++){
+		$nome = $usuarios[$y]['nome'];
+		$email = $usuarios[$y]['email'];
+
+		$html .= "<li>Nome: $nome - Email: $email </li>";
 	}
 
 	return $html;
@@ -39,16 +53,16 @@ function exibeUsuario(){
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Aula 3 - Foreach</title>
+	<title>Aula 3 e 4 - Foreach e For</title>
 </head>
 <body>
 	<h2><?php echo getInfo("titulo");  ?></h2>
 	<p><?php echo getInfo("descricao"); ?></p>
 
 	<ul>
-			<?php
-					echo exibeUsuario();
-			?>
+		<?php
+			echo exibeUsuarioComFor();
+		?>
 	</ul>
 </body>
 </html>
